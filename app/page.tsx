0ed5964e11a4bdfdcda5912149e9a9c7baf90cd9
FoulTrip <davidvasquezmahecha@gmail.com>
2023-07-19 16:00:03 -0500

    act003

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,47 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import iconArrowRight from "@/assets/arrow-right.svg";
 import iconArch from "@/assets/miniaturaArch.png";
+import iconWork from "@/assets/iconDiapositivas.png";
+import iconGithub from "@/assets/iconGithub.svg";
+import iconWeb from "@/assets/iconGlobe.svg";
 
 export default function Home() {
   return (
     <>
-      <h3 className={styles.titleMain}>List of presentation</h3>
+      <div className={styles.banner}>
+        <div className={styles.subBannerOne}>
+          <div className={styles.imgBanner}>
+          <Image className={styles.iconBanner} src={iconWork} alt="iconWork" />
+          </div>
+          <p className={styles.Title}>
+          Swift<span className={styles.titlePart}>Slide</span>
+          </p>
+          <div className={styles.redes}>
+            <Link
+              href="https://portafolio-foultrip.vercel.app/"
+              className={styles.linkRedes}
+            >
+              <Image
+                className={styles.iconRed}
+                src={iconWeb}
+                alt="iconGithub"
+              />
+              <p className={styles.nameAuth}>FoulTrip</p>
+            </Link>
+            <Link
+              href="https://github.com/FoulTrip/Slide-App.git"
+              className={styles.linkRedes}
+            >
+              <Image
+                className={styles.iconRed}
+                src={iconGithub}
+                alt="iconGithub"
+              />
+              <p className={styles.nameAuth}>Repository</p>
+            </Link>
+          </div>
+        </div>
+      </div>
       <div className={styles.BoxPresentation}>
         <div className={styles.card}>
           <div className={styles.iconcard}>
