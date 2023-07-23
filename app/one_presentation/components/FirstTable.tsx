395361d160cf200d27236bdec180114c345333cd
FoulTrip <davidvasquezmahecha@gmail.com>
2023-07-22 22:21:06 -0500

    act004.1

--- app/one_presentation/components/FirstTable.tsx
+++ app/one_presentation/components/FirstTable.tsx
@@ -11,7 +11,7 @@ function FirstTable() {
         </div>
         <div className={styles.textSubBox}>
           <div className={styles.subTextSubBox}>
-            <h3>Arch Linux</h3>
+            <h3 className={styles.titlePrincipal}>Arch Linux</h3>
             <p>Linux distribution for enthusiasts and lovers of customization and total control of their operating system.</p>
           </div>
         </div>
--- app/one_presentation/components/ThreeTable.tsx
+++ app/one_presentation/components/ThreeTable.tsx
@@ -7,15 +7,22 @@ import iconRepository from "../assets/iconRepository.png";
 import iconAUR from "../assets/iconNucleo.png";
 import Image from "next/image";
 import { useState } from "react";
+import FirstElement from "./subComponents/FirstElement";
+import SecondElement from "./subComponents/SecondElement";
+import ThreeElement from "./subComponents/ThreeElement";
+import FourElement from "./subComponents/FourElement";
+import FiveElement from "./subComponents/FiveElement";
+import iconX from "../assets/iconX.svg";
 
 function ThreeTable() {
   const [numberView, setNumberView] = useState(0);
   const [mouseOn, setMouseOn] = useState(false);
   const [clickIcon, setClickIcon] = useState(false);
+  const [selectTopic, setSelectTopic] = useState("");
 
-  const handleContent = () => {
-    setClickIcon(true)
-  }
+  const handleClose = () => {
+    setSelectTopic("");
+  };
 
   return (
     <>
@@ -34,7 +41,10 @@ function ThreeTable() {
               setNumberView(0);
               setMouseOn(false);
             }}
-            onClick={handleContent}
+            onClick={() => {
+              setClickIcon(!clickIcon);
+              setSelectTopic("rolling_release");
+            }}
           >
             <Image
               className={styles.iconSubGroup}
@@ -45,7 +55,7 @@ function ThreeTable() {
 
           {/* Second View */}
           <div
-            className={styles.previw}
+            className={styles.preview}
             onMouseEnter={() => {
               setNumberView(2);
               setMouseOn(true);
@@ -53,6 +63,10 @@ function ThreeTable() {
             onMouseLeave={() => {
               setNumberView(0);
               setMouseOn(false);
+            }}
+            onClick={() => {
+              setClickIcon(!clickIcon);
+              setSelectTopic("custom");
             }}
           >
             <Image
@@ -64,7 +78,7 @@ function ThreeTable() {
 
           {/* Three View */}
           <div
-            className={styles.previw}
+            className={styles.preview}
             onMouseEnter={() => {
               setNumberView(3);
               setMouseOn(true);
@@ -72,6 +86,10 @@ function ThreeTable() {
             onMouseLeave={() => {
               setNumberView(0);
               setMouseOn(false);
+            }}
+            onClick={() => {
+              setClickIcon(!clickIcon);
+              setSelectTopic("learning");
             }}
           >
             <Image
@@ -83,7 +101,7 @@ function ThreeTable() {
 
           {/* Four View */}
           <div
-            className={styles.previw}
+            className={styles.preview}
             onMouseEnter={() => {
               setNumberView(4);
               setMouseOn(true);
@@ -91,6 +109,10 @@ function ThreeTable() {
             onMouseLeave={() => {
               setNumberView(0);
               setMouseOn(false);
+            }}
+            onClick={() => {
+              setClickIcon(!clickIcon);
+              setSelectTopic("performance");
             }}
           >
             <Image
@@ -102,7 +124,7 @@ function ThreeTable() {
 
           {/* Five View */}
           <div
-            className={styles.previw}
+            className={styles.preview}
             onMouseEnter={() => {
               setNumberView(5);
               setMouseOn(true);
@@ -110,6 +132,10 @@ function ThreeTable() {
             onMouseLeave={() => {
               setNumberView(0);
               setMouseOn(false);
+            }}
+            onClick={() => {
+              setClickIcon(!clickIcon);
+              setSelectTopic("repository");
             }}
           >
             <Image
@@ -121,7 +147,7 @@ function ThreeTable() {
 
           {/* Six View */}
           <div
-            className={styles.previw}
+            className={styles.preview}
             onMouseEnter={() => {
               setNumberView(6);
               setMouseOn(true);
@@ -130,52 +156,194 @@ function ThreeTable() {
               setNumberView(0);
               setMouseOn(false);
             }}
+            onClick={() => {
+              setClickIcon(!clickIcon);
+              setSelectTopic("aur");
+            }}
           >
             <Image
               className={styles.iconSubGroup}
               src={iconAUR}
               alt="iconAUR"
             />
-            <div className={styles.nameGroup}></div>
           </div>
         </div>
-        {}
-        
 
-        {numberView == 0 && mouseOn == false ? <p className={styles.nameCharacter}>...</p> : ""}
+        {selectTopic == "rolling_release" ? (
+          <div className={styles.secondSubGroup}>
+            <div className={styles.closeBtn}>
+              <div className={styles.imgClosebtn}>
+                <Image
+                  className={styles.iconX}
+                  src={iconX}
+                  alt="iconX"
+                  onClick={handleClose}
+                />
+              </div>
+            </div>
+            <p className={styles.explain}>
+              It contains a "rolling release" model, which means that it does
+              not have specific versions. Instead, it is constantly updated,
+              providing users with the latest software releases without having
+              to wait for a new version of the distribution.
+            </p>
+          </div>
+        ) : (
+          ""
+        )}
+
+        {selectTopic == "custom" ? (
+          <div className={styles.secondSubGroup}>
+            <div className={styles.closeBtn}>
+              <div className={styles.imgClosebtn}>
+                <Image
+                  className={styles.iconX}
+                  src={iconX}
+                  alt="iconX"
+                  onClick={handleClose}
+                />
+              </div>
+            </div>
+            <p className={styles.explain}>
+              Too flexible and allows users to customize every aspect of the
+              operating system according to their needs. It does not include
+              unnecessary software, allowing users to decide which programs and
+              features they want to install.
+            </p>
+          </div>
+        ) : (
+          ""
+        )}
+
+        {selectTopic == "learning" ? (
+          <div className={styles.secondSubGroup}>
+            <div className={styles.closeBtn}>
+              <div className={styles.imgClosebtn}>
+                <Image
+                  className={styles.iconX}
+                  src={iconX}
+                  alt="iconX"
+                  onClick={handleClose}
+                />
+              </div>
+            </div>
+            <p className={styles.explain}>
+              By installing and configuring Arch Linux from scratch, users can
+              gain a greater understanding of the inner workings of the system
+              and the individual components that make it up. This provides
+              greater control and understanding of their system.
+            </p>
+          </div>
+        ) : (
+          ""
+        )}
+
+        {selectTopic == "performance" ? (
+          <div className={styles.secondSubGroup}>
+            <div className={styles.closeBtn}>
+              <div className={styles.imgClosebtn}>
+                <Image
+                  className={styles.iconX}
+                  src={iconX}
+                  alt="iconX"
+                  onClick={handleClose}
+                />
+              </div>
+            </div>
+            <p className={styles.explain}>
+              Despite being a rolling release distribution, Arch Linux is known
+              for its stability and performance. By allowing users to
+              continually update their system, problems and fixes can be
+              addressed quickly.
+            </p>
+          </div>
+        ) : (
+          ""
+        )}
+
+        {selectTopic == "repository" ? (
+          <div className={styles.secondSubGroup}>
+            <div className={styles.closeBtn}>
+              <div className={styles.imgClosebtn}>
+                <Image
+                  className={styles.iconX}
+                  src={iconX}
+                  alt="iconX"
+                  onClick={handleClose}
+                />
+              </div>
+            </div>
+            <p className={styles.explain}>
+              It has a wide selection of packages in its official repositories
+              and in the community. In addition, packages are updated frequently
+              to keep the system always up to date.
+            </p>
+          </div>
+        ) : (
+          ""
+        )}
+
+        {selectTopic == "aur" ? (
+          <div className={styles.secondSubGroup}>
+            <div className={styles.closeBtn}>
+              <div className={styles.imgClosebtn}>
+                <Image
+                  className={styles.iconX}
+                  src={iconX}
+                  alt="iconX"
+                  onClick={handleClose}
+                />
+              </div>
+            </div>
+            <p className={styles.explain}>
+              In addition to the official repositories, Arch Linux has access to
+              the AUR, a community collection of build scripts that allow you to
+              install and maintain community-developed software. This further
+              expands the catalog of available software.
+            </p>
+          </div>
+        ) : (
+          ""
+        )}
+
+        {numberView == 0 && mouseOn == false ? (
+          <p className={styles.nameCharacter}>...</p>
+        ) : (
+          ""
+        )}
 
         {numberView == 1 && mouseOn == true ? (
-          <p className={styles.nameCharacter}>Rolling Release</p>
+          <p className={styles.nameCharacterRR}>Rolling Release</p>
         ) : (
           ""
         )}
 
         {numberView == 2 && mouseOn == true ? (
-          <p className={styles.nameCharacter}>Customization</p>
+          <p className={styles.nameCharacterC}>Customization</p>
         ) : (
           ""
         )}
 
         {numberView == 3 && mouseOn == true ? (
-          <p className={styles.nameCharacter}>Learning</p>
+          <p className={styles.nameCharacterLrn}>Learning</p>
         ) : (
           ""
         )}
 
         {numberView == 4 && mouseOn == true ? (
-          <p className={styles.nameCharacter}>Performance</p>
+          <p className={styles.nameCharacterPF}>Performance</p>
         ) : (
           ""
         )}
 
         {numberView == 5 && mouseOn == true ? (
-          <p className={styles.nameCharacter}>Large Repositories</p>
+          <p className={styles.nameCharacterLG}>Large Repositories</p>
         ) : (
           ""
         )}
 
         {numberView == 6 && mouseOn == true ? (
-          <p className={styles.nameCharacter}>AUR</p>
+          <p className={styles.nameCharacterAUR}>AUR</p>
         ) : (
           ""
         )}
